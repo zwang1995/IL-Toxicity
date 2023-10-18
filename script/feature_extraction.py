483d9d0d1045bf061a7c620c037417c8de8fedc3
Zihao Wang <55940649+zwang1995@users.noreply.github.com>
2023-10-18 21:36:06 +0200

    Add files via upload

--- script/feature_extraction.py
+++ script/feature_extraction.py
@@ -1,26 +1,5 @@
-from collections import defaultdict
 from rdkit import Chem
 import numpy as np
-import csv
-import json
-
-
-class Molecule_reader():
-    def __init__(self, no, name, smiles, property):
-        self.no = no
-        self.name = name
-        self.smiles = smiles
-        self.property = property
-
-
-def dict_save(path, dict):
-    if isinstance(dict, str):
-        dict = eval(dict)
-    with open(path, 'w', encoding='utf-8') as f:
-        str_ = json.dumps(dict, ensure_ascii=False)
-        # print(type(str_), str_)
-        f.write(str_)
-    print("-> Feature dictionary has been saved.")
 
 
 def iso_identify(atom):
@@ -73,30 +52,3 @@ def Encoding(smiles):
         feature = "".join(["[", symbol, "H", str(H_num), "|", atom_iso, "+" * charge, "-" * -charge, "]", bonds])
         atoms[i] = feature
     return atoms
-
-
-if __name__ == "__main__":
-    csv_file = open("Props.csv", "r")
-    prop_name = "prop"
-    data = csv.DictReader(csv_file)
-    values, items = [], []
-    for row in data:
-        values.append(float(row[prop_name]))
-        item = Molecule_reader(row["Index"], row["name"], row["smiles"],
-                               float(row[prop_name]))
-        items.append(item)
-
-    mean = np.mean(values)
-    std = np.std(values, ddof=0)
-
-    for item in items:
-        item.nor_prop = (item.property - mean) / std
-
-    feature_dict = defaultdict(lambda: len(feature_dict))
-    for item in items:
-        one_hot = Encoding(item.smiles, feature_dict)
-        frequency = [one_hot.count(i) for i in range(len(feature_dict))]
-        item.vector = frequency
-
-    dict_save('Features.txt', feature_dict)
-    print("Number of features: ", len(feature_dict))
